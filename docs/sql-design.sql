1f72e1b019c8811974f8dfa552ca130873054ec6
awlals <awlals616@gmail.com>
2023-01-19 11:27:11 +0900

    helloweb cookies

--- docs/sql-design.sql
+++ docs/sql-design.sql
@@ -3,3 +3,7 @@ desc user;
 select * from user;
 -- join
 insert into user values(null, 'test', 'test@test.com', password('test'),'male',now());
+
+-- login
+-- 해당되는 값을 반환시켜주기 
+select no, name from user where email='test03@test.com' and password = password('test');
