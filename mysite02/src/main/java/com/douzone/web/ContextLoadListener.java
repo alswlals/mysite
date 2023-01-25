5291b8b44abf9d506b3d9a6acb8d1177735eb889
awlals <awlals616@gmail.com>
2023-01-25 13:55:41 +0900

    contextConfigLocation #1

--- mysite02/src/main/java/com/douzone/web/ContextLoadListener.java
+++ mysite02/src/main/java/com/douzone/web/ContextLoadListener.java
@@ -9,7 +9,7 @@ public class ContextLoadListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent sce) {
 		ServletContext sc = sce.getServletContext();
 		String contextConfigLocation = sc.getInitParameter("contextConfigLocation");
-		System.out.println("Application Starts...");
+		System.out.println("Application Starts..." + contextConfigLocation);
 	}
 
 	public void contextDestroyed(ServletContextEvent sce) {
