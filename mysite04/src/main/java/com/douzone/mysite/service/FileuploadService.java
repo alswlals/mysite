41cbd2fbdc21bd7c6e33ad1da2e1f39b1fd410d2
awlals <awlals616@gmail.com>
2023-02-14 11:17:24 +0900

    mysite05: ServletContainerInitializer #1

--- mysite04/src/main/java/com/douzone/mysite/service/FileuploadService.java
+++ mysite04/src/main/java/com/douzone/mysite/service/FileuploadService.java
@@ -6,22 +6,26 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Calendar;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.douzone.mysite.exception.FileuploadServiceException;
 
 @Service
+@PropertySource("classpath:com/douzone/mysite/web/fileupload.properties")
 public class FileuploadService {
 	/*propertis 빼*/
-	private static String SAVE_PATH = "/Users/awlals/Documents/douzone2023/eclipse/eclipse-workspace/mysite/mysite-uploads";
-	private static String URL_PATH = "/assets/upload-images";
-
+	@Autowired
+	private Environment env;
+	
 	public String restore(MultipartFile file) {
 		String url = null;
 
 		try {
-			File uploadDirectory = new File(SAVE_PATH);
+			File uploadDirectory = new File(env.getProperty("fileupload.uploadLocation"));
 			if(!uploadDirectory.exists()) {
 				uploadDirectory.mkdirs();
 			}
@@ -41,11 +45,11 @@ public class FileuploadService {
 
 			byte[] data = file.getBytes();
 
-			OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFilename);
+			OutputStream os = new FileOutputStream(env.getProperty("fileupload.uploadLocation") + "/" + saveFilename);
 			os.write(data);
 			os.close();
 			
-			url = URL_PATH + "/" + saveFilename;
+			url = env.getProperty("fileupload.resourceUrl") + "/" + saveFilename;
 			
 		} catch (IOException ex) {
 			throw new FileuploadServiceException(ex.toString());
