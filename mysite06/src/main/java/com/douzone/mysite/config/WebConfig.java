c58c613a9f3645c7d2d852d82dbe6b1b89f979e2
awlals <awlals616@gmail.com>
2023-02-16 16:28:15 +0900

    mysite06 css 전 단계

--- mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.douzone.mysite.config.web;
+package com.douzone.mysite.config;
 
 import java.util.List;
 
@@ -16,49 +16,56 @@ import com.douzone.mysite.security.LoginInterceptor;
 import com.douzone.mysite.security.LogoutInterceptor;
 
 @SpringBootConfiguration
-public class WebConfig  implements WebMvcConfigurer {
-
-	/* Argument Resolver */
+public class WebConfig implements WebMvcConfigurer {
+	// Argument Resolver
 	@Bean
 	public HandlerMethodArgumentResolver handlerMethodArgumentResolver() {
 		return new AuthUserHandlerMethodArgumentResolver();
 	}
-	
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(handlerMethodArgumentResolver());
 	}
-	
-	/* Site Interceptor*/
+
+	// Site Inteceptor
 	@Bean
 	public HandlerInterceptor siteInterceptor() {
 		return new SiteInterceptor();
 	}
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(siteInterceptor()).addPathPatterns("/**");
-		registry.addInterceptor(loginInterceptor()).addPathPatterns("/user/auth");
-		registry.addInterceptor(logoutInterceptor()).addPathPatterns("/user/logout");
-		registry.addInterceptor(authInterceptor()).addPathPatterns("/**")
-				.excludePathPatterns("/user/auth", "/user/logout", "/assets/**");
-	}
-	
-	/* Interceptors*/
+	// Security Interceptors
 	@Bean
 	public HandlerInterceptor loginInterceptor() {
 		return new LoginInterceptor();
 	}
-	
+
 	@Bean
 	public HandlerInterceptor logoutInterceptor() {
 		return new LogoutInterceptor();
 	}
-	
+
 	@Bean
 	public HandlerInterceptor authInterceptor() {
 		return new AuthInterceptor();
 	}
 
-	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+			.addInterceptor(loginInterceptor())
+			.addPathPatterns("/user/auth");
+		
+		registry
+			.addInterceptor(logoutInterceptor())
+			.addPathPatterns("/user/logout");
+
+		registry
+			.addInterceptor(authInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns("/user/auth", "/user/logout", "/assets/**");
+
+		registry
+			.addInterceptor(siteInterceptor())
+			.addPathPatterns("/**");
+	}
 }
--- mysite06/src/main/java/com/douzone/mysite/event/ApplicationContextEventListener.java
+++ mysite06/src/main/java/com/douzone/mysite/event/ApplicationContextEventListener.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.douzone.mysite.service.SiteService;
 import com.douzone.mysite.vo.SiteVo;
-
+@Component
 public class ApplicationContextEventListener {
 	
 	@Autowired
@@ -23,11 +25,12 @@ public class ApplicationContextEventListener {
 		System.out.println(System.identityHashCode(applicationContext));
 		System.out.println("--- Context Refresh Event Received --- : " + applicationContext);
 		
+		InternalResourceViewResolver viewResolver = applicationContext.getBean(InternalResourceViewResolver.class);
+		viewResolver.setExposeContextBeansAsAttributes(true);
+		viewResolver.setExposedContextBeanNames("site");
+		
 		SiteService service = applicationContext.getBean(SiteService.class);
 		SiteVo site = service.getSite();
-		
-		AutowireCapableBeanFactory factory = applicationContext.getAutowireCapableBeanFactory();
-		BeanDefinitionRegistry registry = (BeanDefinitionRegistry) factory;
 		
 		MutablePropertyValues propertyValues = new MutablePropertyValues();
 		propertyValues.add("title", site.getTitle());
@@ -35,13 +38,14 @@ public class ApplicationContextEventListener {
 		propertyValues.add("welcome", site.getWelcome());
 		propertyValues.add("description", site.getDescription());
 		
-		
-		
 		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
 		beanDefinition.setBeanClass(SiteVo.class);
 		beanDefinition.setPropertyValues(propertyValues);
 		
+		AutowireCapableBeanFactory factory = applicationContext.getAutowireCapableBeanFactory();
+		BeanDefinitionRegistry registry = (BeanDefinitionRegistry)factory;
 		registry.registerBeanDefinition("site", beanDefinition);
+		
 		
 	}
 	
